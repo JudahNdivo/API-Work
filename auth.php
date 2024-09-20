589e09892f4fb261b63850a93a4fa8d71dd6534b
JudahNdivo <judah.ndivo@strathmore.edu>
2024-09-20 12:03:24 +0300

    Commented on what needs to be implemented on the sign up page

--- auth.php
+++ auth.php
@@ -9,6 +9,18 @@ class auth{
         $username = $_POST["username"];
 
         //die ($username);
+
+        //Sanitize the data
+        //Verify that the fullname has only letters, space, dash and apostrophe
+        //Verify that the email address is valid
+        //Verify that email domain is authorized(strathmore.edu, gmail.com, yahoo.com, hotmail.com)
+        //Verify if the email address is already registered
+        //Verify that the username already exists
+        //Implement 2FA (email => PHPMailer)
+        //Send email verification with a OTP (One Time Password) 
+        //Verify that the password is identical to the repeat password
+        //Verify that the password length is between 4 and 8 characters
+
         $cols = ['fullname', 'email', 'username']; //key
         $vals = [$fullname, $email_address, $username]; //value
 
